0c3edeea5b38dbc9b2389653e578892c67ea5214
Shannon Smith <infiltratr@yandex.com>
2026-09-01 23:55:27 +1000

    Add native iPhone GUI diagnostics

---
+++ platform/apple/FordLinkBLETransport.h
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+#import "../../src/link/platform/apple/LinkBLETransport.h"
+#define FordLinkBLETransport LinkBLETransport
+#define FordLinkBLETransportDelegate LinkBLETransportDelegate
+typedef LinkBLETransportState FordLinkBLETransportState;
