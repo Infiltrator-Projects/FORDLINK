99d8b65cc7a5ae4b07889b8172ffbdcfe8753d99
Shannon Smith <infiltratr@yandex.com>
2026-09-01 23:50:54 +1000

    Replace CLI skeleton with cross-platform GUI product

--- include/fordlink/fordlink.h
+++ include/fordlink/fordlink.h
@@ -1,20 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 #ifndef FORDLINK_H
 #define FORDLINK_H
-
-#ifdef __cplusplus
-extern "C" {
+#ifndef FORDLINK_VERSION
+#define FORDLINK_VERSION "0.2.0"
 #endif
-
-#define FORDLINK_VERSION "0.1.1"
 #define FORDLINK_BRAND "Ford"
-
 const char *fordlink_product_name(void);
 const char *fordlink_brand_name(void);
 const char *fordlink_version(void);
-
-#ifdef __cplusplus
-}
-#endif
-
 #endif
