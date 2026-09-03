b0ac04cc8f0829d6d902ef2df005febeefd4ffc4
Shannon Smith <infiltratr@yandex.com>
2026-09-03 18:21:33 +1000

    Release 0.2.16

--- include/fordlink/fordlink.h
+++ include/fordlink/fordlink.h
@@ -2,7 +2,7 @@
 #ifndef FORDLINK_H
 #define FORDLINK_H
 #ifndef FORDLINK_VERSION
-#define FORDLINK_VERSION "0.2.15"
+#define FORDLINK_VERSION "0.2.16"
 #endif
 #define FORDLINK_BRAND "Ford"
 const char *fordlink_product_name(void);
